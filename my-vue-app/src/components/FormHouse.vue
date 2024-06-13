c8b9cad97ae75291f55b1f6df51e85dadf9581fe
nenastiana <politova.anastasiia@gmail.com>
2024-06-13 23:59:45 +0200

    moved house form logic to its component

--- my-vue-app/src/components/FormHouse.vue
+++ my-vue-app/src/components/FormHouse.vue
@@ -105,7 +105,6 @@
 
 <script setup>
 import { ref } from 'vue';
-import { createHouseForm, isValidHouseForm } from '../helpers/house.js';
 
 import UploadImage from '../components/UploadImage.vue';
 
@@ -126,6 +125,44 @@ const props = defineProps({
 const house = props.house;
 const houseForm = ref(createHouseForm(house));
 const showError = ref(false);
+
+function createHouseForm(house) {
+    return {
+        price: house ? house.price : '',
+        image: house ? house.image : '',
+        bedrooms: house ? house.bedrooms : '',
+        bathrooms: house ? house.bathrooms : '',
+        size: house ? house.size : '',
+        streetName: house ? house.streetName : '',
+        houseNumber: house ? house.houseNumber : '',
+        numberAddition: house ? house.numberAddition : '',
+        city: house ? house.city : '',
+        zip: house ? house.zip : '',
+        constructionYear: house ? house.constructionYear : '',
+        hasGarage: house ? house.hasGarage : '',
+        description: house ? house.description : ''
+    };
+};
+
+function isValidHouseForm(houseForm) {
+    if (!houseForm || !houseForm.value) {
+        return false;
+    }
+    const year = parseInt(houseForm.value.constructionYear, 10);
+    return (
+        houseForm.value.price &&
+        houseForm.value.bedrooms &&
+        houseForm.value.bathrooms &&
+        houseForm.value.size &&
+        houseForm.value.streetName &&
+        houseForm.value.houseNumber &&
+        houseForm.value.city &&
+        houseForm.value.zip &&
+        houseForm.value.constructionYear &&
+        year >= 1901 &&
+        houseForm.value.description
+    );
+};
 
 const submitFormFunc = () => {
     showError.value = true;
