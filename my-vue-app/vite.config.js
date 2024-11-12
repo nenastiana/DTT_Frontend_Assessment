7e30a88efee2a393864f0818c2fd7d3cb1b7c77d
nenastiana <politova.anastasiia@gmail.com>
2024-11-12 22:58:55 +0100

    set default port 8080 vite.config.js

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -6,6 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
+  base: '/DTT_Frontend_Assessment/',
   plugins: [
     vue(),
     vueJsx(),
